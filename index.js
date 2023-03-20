6f702e000d00b8e00109b826a8f6393f45b84368
Arthur Ribeiro <110831236+arthuribeirojn@users.noreply.github.com>
2023-03-20 10:58:06 -0300

    usando forEach e if para buscar informações input

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-var nome = document.querySelector("#exampleInputName");
-var genero = document.querySelector("#exampleInputGenderM");
-var genero = document.querySelector("#exampleInputGenderF");
-var nascimento = document.querySelector("#exampleInputBirth");
-var pais = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail1");
-var senha = document.querySelector("#exampleInputPassword1");
-var foto = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#admin");
-var salvar = document.querySelector(".btn btn-success");
+/*var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll(
+  "#form-user-create [name=gender]:checked"
+);
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");*/
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+fields.forEach( (field, index)=> {
+
+    if(field.name == "gender"){
+
+        if(field.checked){
+            console.log("SIM", field)
+        }
+        
+    }else{
+
+        console.log("NAO", field)
+
+    }
+  console.log(field.name);
+
+});
